8c563f72148a9581fe6f97146b39e7e3b078fa80
Evan Xiang <109013894+evankxiang@users.noreply.github.com>
2023-10-06 10:50:54 -0400

    Update Parabola.java

--- Parabola/src/parabola/Parabola.java
+++ Parabola/src/parabola/Parabola.java
@@ -19,7 +19,7 @@ public class Parabola extends SSAWindow
     static final int HEIGHT = 600;
     int NUMSEGMENTS = 1200;
     static Scanner input = new Scanner(System.in);
-    static final double INCREMENT = 0.1;
+    static final double INCREMENT = 0.01;
     static int aval;
     static int bval;
     static int cval;
@@ -30,7 +30,7 @@ public class Parabola extends SSAWindow
         canvas.drawParabola();
     }
 
-    //
+    //method to get input from the user, intializes static variables(aval, bval, cval), prints/displays equations
     void getInput()
     {
         drawString("Standard form of the Quadratic is " + "ax^2+bx+c", 20,30);
@@ -47,7 +47,7 @@ public class Parabola extends SSAWindow
         drawString("Your Equation Is: " + aval + "x^2+" + bval + "x+" + cval,20,70);
     }
 
-    //
+    //draws the axes using a for loop for the X-axis and Y-axis
     void drawAxes()
     {
         for(int x = 1;x<=10000;x+=20)
@@ -55,22 +55,25 @@ public class Parabola extends SSAWindow
         for(int y = 1;y<=10000;y+=20)
             drawLine(300,y,300,y+20);
     }
+    //converts unit coords on the cartesian plane to screen on X-axis
     static int worldToScreenX(double x)
     {
         int screenX = (int) (WIDTH/2 * x) + WIDTH/2;
         return screenX;
     }
-
+ //converts unit coords on the cartesian plane to screen on Y-axis
     static int worldToScreenY(double y)
     {
-        int screenY =  (int) (WIDTH/2 * y) + WIDTH/2;
+        int screenY =  (int) (-HEIGHT/2 * y) + HEIGHT/2;
         return screenY;
     }
+//find the yval of every point using the standard form of the quadratic
     static double f1(double x){
         double yval;
         yval = aval*Math.pow(x,2.0)+bval*x+cval;
         return yval;
     }
+//calls all above methods needed and plots the points according to preset increment variable(0.01)
     void drawParabola()
     {
         drawAxes();
@@ -78,21 +81,11 @@ public class Parabola extends SSAWindow
         for(double i = -1.0;i<=1.0;i+=INCREMENT){
             putPixel(worldToScreenX(i), worldToScreenY(f1(i)));
         }
-        }
-            
-    
-
-
+    }
     Parabola(int width, int height)
     {
         super(width, height);
     }
-
-    // draw an arc centered at unit (x, y) from angle 0.0 to "angle" (radians)
-    void drawArc(double x, double y, double angle, double length)
-    {
-    }
-
     // use this for keyPress input
     public void keyPressed(int code)
     {
